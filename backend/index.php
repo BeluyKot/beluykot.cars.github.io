2b30286cf454a5f5c5d782c83a5ff89b6dfbe555
BeluyKot <144977176+BeluyKot@users.noreply.github.com>
2023-12-20 09:59:59 +0200

    Add files via upload

---
+++ backend/index.php
@@ -0,0 +1,17 @@
+<?php
+echo "hallo " . $_GET["name"];
+//
+//$a = ['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'];
+//
+//function myFunc($array, $search)
+//{
+//    твой кот(д)
+//}
+//
+//myFunc();
+//
+?>
+
+<!--<div>-->
+<!--<span style="color: white">--><?php //echo $name?><!--</span>-->
+<!--</div>-->
